4eb3f6617f45c9c4967dd44d45539b0770a4059f
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2021-11-02 12:13:26 -0400

    Add files via upload

---
+++ index123.js
@@ -0,0 +1,42 @@
+(function () {
+	// body...
+
+	var margin = {top:0, left:0, right:0, bottom:0},
+	height = 400 - margin.top - margin.bottom,
+	width = 800 - margin.left - margin.right;
+
+
+	var svg = d3.select("#map")
+		.append("svg")
+		.attr("height", height + margin.top + margin.bottom)
+		.attr("width", width + margin.left + margin.right)
+		.append("g")
+		.attr("transform", "translate("+ margin.left +", "+margin.top +")");
+
+
+		d3.queue()
+	.defer(d3.json, "us.json")
+	.await(ready)
+
+var projection = d3.geoAlbersUsa()
+	.translate([width / 2, height / 2])
+	.scale(850)
+
+var path = d3.geoPath()
+	.projection(projection)
+
+function ready (error, data) {
+	console.log(data)
+
+	var states = topojson.feature(data, data.objects.states).feaatures
+console.log(states)
+
+svg.selectAll(".state")
+	.data(states)
+	.enter().append("path")
+	.attr("class", "state")
+	.attr("d", path)
+
+}
+
+})();
